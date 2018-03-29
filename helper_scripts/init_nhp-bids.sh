130310f98d26ae1b30380bb0d1c7b8afb91a4305
Chris Klink <p.c.klink@gmail.com>
2018-03-29 11:06:54 +0200

    Folder for helper scripts in running e.g. batch jobs

---
+++ helper_scripts/init_nhp-bids.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -R -f ~/NHP-BIDS
+git clone https://github.com/VisionandCognition/NHP-BIDS.git
+ln -s /nfs/cortalg/NHP-BIDS/derivatives ~/NHP-BIDS/derivatives
+ln -s /nfs/cortalg/NHP-BIDS/sourcedata ~/NHP-BIDS/sourcedata
+ln -s /nfs/cortalg/NHP-BIDS/sub-danny ~/NHP-BIDS/sub-danny
+ln -s /nfs/cortalg/NHP-BIDS/sub-eddy ~/NHP-BIDS/sub-eddy
+ln -s /nfs/cortalg/NHP-BIDS/workingdirs ~/NHP-BIDS/workingdirs
+mkdir ~/NHP-BIDS/job-logs
