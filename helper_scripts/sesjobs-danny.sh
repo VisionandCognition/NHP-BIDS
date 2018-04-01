761414e601b79d39f4bd49d907f9503b5d2e96aa
Chris Klink <p.c.klink@gmail.com>
2018-04-01 13:46:12 +0200

    Remove pbs flag on jobs

--- helper_scripts/sesjobs-danny.sh
+++ helper_scripts/sesjobs-danny.sh
@@ -4,7 +4,7 @@ echo 'Submitting sessions as separate jobs...'
 #qsub ~/NHP-BIDS/code/pbs/preprocess-danny-20171129.job
 #qsub ~/NHP-BIDS/code/pbs/preprocess-danny-20171207.job
 #qsub ~/NHP-BIDS/code/pbs/preprocess-danny-20171214.job
-qsub ~/NHP-BIDS/code/pbs/preprocess-danny-20171220.job
+#qsub ~/NHP-BIDS/code/pbs/preprocess-danny-20171220.job
 qsub ~/NHP-BIDS/code/pbs/preprocess-danny-20180124.job
 qsub ~/NHP-BIDS/code/pbs/preprocess-danny-20180125.job
 qsub ~/NHP-BIDS/code/pbs/preprocess-danny-20180131.job
