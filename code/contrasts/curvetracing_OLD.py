234bbada6c15e321463c6ed503866fd2b39f5772
Chris Klink <p.c.klink@gmail.com>
2019-02-15 09:56:42 +0100

    more data and contrasts added

--- code/contrasts/curvetracing_OLD.py
+++ code/contrasts/curvetracing_OLD.py
@@ -1,20 +1,5 @@
 # Curve tracing contrasts
 contrasts = [
-    ['AttendUL_COR>CurveNoResponse', 'T',  # t-test
-     ['AttendUL_COR','CurveNoResponse'],
-     [1.0, -1.0],
-    ['AttendDL_COR>CurveNoResponse', 'T',  # t-test
-     ['AttendDL_COR','CurveNoResponse'],
-     [1.0, -1.0],
-    ['AttendUR_COR>CurveNoResponse', 'T',  # t-test
-     ['AttendUR_COR','CurveNoResponse'],
-     [1.0, -1.0],
-    ['AttendDR_COR>CurveNoResponse', 'T',  # t-test
-     ['AttendDR_COR','CurveNoResponse'],
-     [1.0, -1.0],
-    ['Correct>Incorrect', 'T',  # t-test
-     ['AttendUL_COR','AttendDL_COR','AttendUR_COR','AttendDR_COR','CurveNotCOR'],
-     [1.0, 1.0, 1.0, 1.0, -4.0],
     ['Curves>Baseline', 'T',  # t-test
      ['AttendUL_COR', 'AttendDL_COR', 'AttendUR_COR', 'AttendDR_COR',
       'AttendCenter_COR'],
@@ -47,20 +32,3 @@ contrasts = [
      ['Reward'],
      [1.0]],
 ]
-
-
-#   0   UL > No response
-#   1   DL > No response
-#   2   UR > No response
-#   3   DR > No response
-#   4   Correct > Incorrect (false, miss, break)
-#   5   All Curves > Base
-#   6   Attend Left > Attend Right
-#   7   Attend Right > Attend Left
-#   8   UL > other
-#   9   DL > other
-#  10   UR > other
-#  11   DR > other
-#  12   Hand Left > Hand Right
-#  13   Hand Right > Hand Left
-#  14   Reward > baseline       
