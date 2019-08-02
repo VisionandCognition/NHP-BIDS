70243cf9591ac0bfbbf9b1a11642c994ee2d373e
Chris Klink <p.c.klink@gmail.com>
2019-08-02 09:15:08 +0200

    updated links to project space

--- helper_scripts/init_nhp-bids.sh
+++ helper_scripts/init_nhp-bids.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 rm -R -f ~/NHP-BIDS
 git clone https://github.com/VisionandCognition/NHP-BIDS.git
-ln -s /nfs/cortalg/NHP-BIDS/derivatives ~/NHP-BIDS/derivatives
-ln -s /nfs/cortalg/NHP-BIDS/sourcedata ~/NHP-BIDS/sourcedata
-ln -s /nfs/cortalg/NHP-BIDS/manual-masks ~/NHP-BIDS/manual-masks
-ln -s /nfs/cortalg/NHP-BIDS/sub-danny ~/NHP-BIDS/sub-danny
-ln -s /nfs/cortalg/NHP-BIDS/sub-eddy ~/NHP-BIDS/sub-eddy
-ln -s /nfs/cortalg/NHP-BIDS/workingdirs ~/NHP-BIDS/workingdirs
+ln -s /project/cortalg/NHP-BIDS/derivatives ~/NHP-BIDS/derivatives
+ln -s /project/cortalg/NHP-BIDS/sourcedata ~/NHP-BIDS/sourcedata
+ln -s /project/cortalg/NHP-BIDS/manual-masks ~/NHP-BIDS/manual-masks
+ln -s /project/cortalg/NHP-BIDS/sub-danny ~/NHP-BIDS/sub-danny
+ln -s /project/cortalg/NHP-BIDS/sub-eddy ~/NHP-BIDS/sub-eddy
+ln -s /project/cortalg/NHP-BIDS/workingdirs ~/NHP-BIDS/workingdirs
 mkdir ~/NHP-BIDS/job-logs
 mkdir ~/NHP-BIDS/logs
 chmod -R 777 ~/NHP-BIDS # this appears necessary to avoid permission errors
