148e5e98682a1f23cd791bdec36972ced1faea83
Chris Klink <p.c.klink@gmail.com>
2019-11-01 13:42:36 +0100

    Bug fixes

--- code/copy-to-bids_template.sh
+++ code/copy-to-bids_template.sh
@@ -12,6 +12,9 @@
 # files with get run numbers in the order they are in the array
 # for functionals (+ beh/eye), run numbers should be explixitly defined
 
+# subfolder where the scan files are located
+SF=MRI
+
 # ==========================================================================
 # STRUCTURAL
 # ==========================================================================
@@ -19,26 +22,26 @@
 
 # T1 ----
 declare -a T1s=(\
-    T1_NoSENSE_NHP_20190213141308_501.nii.gz
+    T1.nii.gz
     )
 
 # T2 ----
 declare -a T2s=(\
-    T2_TRA_20190213141308_1301.nii.gz
+    T2.nii.gz
     )
 
 # Fieldmap ----
 declare -a fmap_mag=(\
-    B0_EPI_20190213141308_2801_e1a.nii.gz
+    B0_e1a.nii.gz
     )
 
 declare -a fmap_phase=(\
-    B0_EPI_20190213141308_2801_e1.nii.gz
+    B0_e1.nii.gz
     )
 
 # DWI ----
 declare -a DWI=(\
-    DTI64_20190213141308_2301.nii.gz
+    DTI64.nii.gz
     )
 
 # ==========================================================================
@@ -52,34 +55,29 @@ declare -a func_runnr=( 01 02 03 )
 # curvetracing / curvetracinginccentral / ctcheckerboard 
 # prf /rest / figgnd / figgndloc / checkerHRF / naturalmovie
 declare -a task=( \
-    curvetracing \
-    curvetracing \
-    curvetracing \
+    xxx 
+    xxx 
     )
 # MRI-data functional runs
 declare -a funcs=( \
-    run001_CurveTrace_TR2.5s_20190213141308_301.nii.gz \
-    run002_CurveTrace_TR2.5s_20190213141308_601.nii.gz \
-    run003_CurveTrace_TR2.5s_20190213141308_801.nii.gz \
+    xxx
+    xxx
     )
 # Corresponding topup-scans
 # when these exist there should be the same number of entries as the functional runs
 declare -a topup=( \
-    run001_topup_20190213141308_401.json \
-    run002_topup_20190213141308_701.nii.gz \
-    run003_topup_20190213141308_901.nii.gz \
+    xxx
+    xxx
     )
 # behavioral log folders
 declare -a behs=( \
-    Danny_Curve_Tracing_StimSettings_3T_Spinoza_3T_20190213T1432-T1442.50 \
-    Danny_Curve_Tracing_StimSettings_3T_Spinoza_3T_20190213T1450-T1501.48 \
-    Danny_Curve_Tracing_StimSettings_3T_Spinoza_3T_20190213T1503-T1512.55 \
+    xxx
+    xxx
     )
 # eye tracking files
 declare -a eyes=( \
-    Danny_20190213T1432.tda \
-    Danny_20190213T1450.tda \
-    Danny_20190213T1503.tda \
+    xxx
+    xxx 
     )
 
 
@@ -112,11 +110,19 @@ BIDS_DEST="${BIDS_DEST/2017/ses-2017}"
 BIDS_DEST="${BIDS_DEST/2018/ses-2018}"
 BIDS_DEST="${BIDS_DEST/2019/ses-2019}"
 BIDS_DEST="${BIDS_DEST/2019/ses-2019}"
+BIDS_DEST="${BIDS_DEST/2020/ses-2020}"
+BIDS_DEST="${BIDS_DEST/2021/ses-2021}"
+BIDS_DEST="${BIDS_DEST/2022/ses-2022}"
+
 # sourcedata ----
 BIDS_UNPROC_DEST="${BIDS_DEST/NHP-BIDS/NHP-BIDS/sourcedata}"
 
 # some feedback to terminal ----
-echo "Copying BIDS data to $BIDS_DEST and $BIDS_UNPROC_DEST..."
+echo '=============================================='
+echo "Copying BIDS data to" 
+echo $BIDS_DEST 
+echo $BIDS_UNPROC_DEST
+echo '=============================================='
 
 # create some useful variables ----
 date=$(basename $(pwd))
@@ -134,33 +140,37 @@ bses="ses-${date}"
 #                                              
 # ==========================================================================
 
-echo "Copying anatomicals (if they exist)"
+echo "Copying anatomicals (if they exist) ---"
 runnr=0
 if [ ${#T1s[@]} -eq 0 ]; then
+    echo 'No T1s declared'
+else
     echo 'T1 scans'
     mkdir -p "$BIDS_UNPROC_DEST/anat"
     for i in "${!T1s[@]}"; do
         runnr=$((runnr + 1))
         base=$(basename ${T1s[$i]} .nii.gz)
-        cp -n MRI/NII/${base}.nii.gz "$BIDS_UNPROC_DEST/anat/${bsub}_${bses}_acq-nosense_run-${runnr[$i]}_T1w.nii.gz"
+        cp -n ${SF}/${base}.nii.gz "$BIDS_UNPROC_DEST/anat/${bsub}_${bses}_acq-nosense_run-${runnr[$i]}_T1w.nii.gz"
         # if there are json files copy them too
-        if [ -f MRI/NII/${base}.json]; then
-            cp -n MRI/NII/${base}.json "$BIDS_UNPROC_DEST/anat/${bsub}_${bses}_acq-nosense_run-${runnr[$i]}_T1w.json"
+        if [ -f ${SF}/${base}.json ]; then
+            cp -n ${SF}/${base}.json "$BIDS_UNPROC_DEST/anat/${bsub}_${bses}_acq-nosense_run-${runnr[$i]}_T1w.json"
         fi
     done
 fi
 
 runnr=0
 if [ ${#T2s[@]} -eq 0 ]; then
+    echo 'No T2s declared'
+else
     echo 'T2 scans'
     mkdir -p "$BIDS_UNPROC_DEST/anat"
     for i in "${!T2s[@]}"; do
         runnr=$((runnr + 1))
         base=$(basename ${T2s[$i]} .nii.gz)
-        cp -n MRI/NII/${base}.nii.gz "$BIDS_UNPROC_DEST/anat/${bsub}_${bses}_acq-nosense_run-${runnr[$i]}_T2w.nii.gz"
+        cp -n ${SF}/${base}.nii.gz "$BIDS_UNPROC_DEST/anat/${bsub}_${bses}_acq-nosense_run-${runnr[$i]}_T2w.nii.gz"
         # if there are json files copy them too
-        if [ -f MRI/NII/${base}.json]; then
-            cp -n  MRI/NII/${base}.json "$BIDS_UNPROC_DEST/anat/${bsub}_${bses}_acq-nosense_run-${runnr[$i]}_T2w.json"
+        if [ -f ${SF}/${base}.json ]; then
+            cp -n  ${SF}/${base}.json "$BIDS_UNPROC_DEST/anat/${bsub}_${bses}_acq-nosense_run-${runnr[$i]}_T2w.json"
         fi
     done
 fi
@@ -174,33 +184,37 @@ fi
 #                               |_|   
 # ==========================================================================
 
-echo "Copying fieldmaps (if they exist)"
+echo "Copying fieldmaps (if they exist) ---"
 runnr=0
 if [ ${#fmap_mag[@]} -eq 0 ]; then
+    echo 'No B0 magnitude files declared'
+else
     echo 'Magnitude'
     mkdir -p "$BIDS_UNPROC_DEST/fmap"
     for i in "${!fmap_mag[@]}"; do
         runnr=$((runnr + 1))
         base=$(basename ${fmap_mag[$i]} .nii.gz)
-        cp -n MRI/NII/${base}.nii.gz "$BIDS_UNPROC_DEST/fmap/${bsub}_${bses}_magnitude1.nii.gz"
+        cp -n ${SF}/${base}.nii.gz "$BIDS_UNPROC_DEST/fmap/${bsub}_${bses}_magnitude1.nii.gz"
         # if there are json files copy them too
-        if [ -f MRI/NII/${base}.json]; then
-            cp -n  MRI/NII/${base}.json "$BIDS_UNPROC_DEST/fmap/${bsub}_${bses}_magnitude1.json"
+        if [ -f ${SF}/${base}.json ]; then
+            cp -n  ${SF}/${base}.json "$BIDS_UNPROC_DEST/fmap/${bsub}_${bses}_magnitude1.json"
         fi
     done
 fi
 
 runnr=0
 if [ ${#fmap_phase[@]} -eq 0 ]; then
-    echo 'Magnitude'
+    echo 'No B0 phase files declared'
+else
+    echo 'Phase'
     mkdir -p "$BIDS_UNPROC_DEST/fmap"
     for i in "${!fmap_phase[@]}"; do
         runnr=$((runnr + 1))
         base=$(basename ${fmap_phase[$i]} .nii.gz)
-        cp -n MRI/NII/${base}.nii.gz "$BIDS_UNPROC_DEST/fmap/${bsub}_${bses}_phasediff.nii.gz"
+        cp -n ${SF}/${base}.nii.gz "$BIDS_UNPROC_DEST/fmap/${bsub}_${bses}_phasediff.nii.gz"
         # if there are json files copy them too
-        if [ -f MRI/NII/${base}.json]; then
-            cp -n  MRI/NII/${base}.json "$BIDS_UNPROC_DEST/fmap/${bsub}_${bses}_phasediff.json"
+        if [ -f ${SF}/${base}.json ]; then
+            cp -n  ${SF}/${base}.json "$BIDS_UNPROC_DEST/fmap/${bsub}_${bses}_phasediff.json"
         fi
     done
 fi
@@ -214,22 +228,25 @@ fi
 #                
 # ==========================================================================
 
-echo "Copying dwi (if they exist)"
+echo "Copying dwi (if they exist) ---"
 runnr=0
 if [ ${#DWI[@]} -eq 0 ]; then
-    mkdir -p "$BIDS_UNPROC_DEST/dwi"
+    echo 'No DWI files declared'
+else
+    echo 'DWIs'    mkdir -p "$BIDS_UNPROC_DEST/dwi"
     for i in "${!DWI[@]}"; do
         runnr=$((runnr + 1))
         base=$(basename ${DWI[$i]} .nii.gz)
-        cp -n MRI/NII/${base}.nii.gz "$BIDS_UNPROC_DEST/dwi/${bsub}_${bses}_run-${runnr}_dwi.nii.gz"
-        cp -n MRI/NII/${base}.bval  "$BIDS_UNPROC_DEST/dwi/${bsub}_${bses}_run-${runnr}_dwi.bval"
-        cp -n MRI/NII/${base}.bvec  "$BIDS_UNPROC_DEST/dwi/${bsub}_${bses}_run-${runnr}_dwi.bvec"
+        cp -n ${SF}/${base}.nii.gz "$BIDS_UNPROC_DEST/dwi/${bsub}_${bses}_run-${runnr}_dwi.nii.gz"
+        cp -n ${SF}/${base}.bval  "$BIDS_UNPROC_DEST/dwi/${bsub}_${bses}_run-${runnr}_dwi.bval"
+        cp -n ${SF}/${base}.bvec  "$BIDS_UNPROC_DEST/dwi/${bsub}_${bses}_run-${runnr}_dwi.bvec"
         # if there are json files copy them too
-        if [ -f MRI/NII/${base}.json]; then
-            cp -n  MRI/NII/${base}.json "$BIDS_UNPROC_DEST/dwi/${bsub}_${bses}_run-${runnr}_dwi.json"
+        if [ -f ${SF}/${base}.json ]; then
+            cp -n  ${SF}/${base}.json "$BIDS_UNPROC_DEST/dwi/${bsub}_${bses}_run-${runnr}_dwi.json"
         fi
     done
 fi
+
 
 
 # ==========================================================================
@@ -240,35 +257,41 @@ fi
 #                                             
 # ==========================================================================
 
-echo "Copying functionals (if they exist)"
+echo "Copying functionals (if they exist) ---"
 if [ ${#funcs[@]} -eq 0 ]; then
+    echo 'No functionals declared'
+else
+    echo 'EPIs'    
     mkdir -p "$BIDS_UNPROC_DEST/func"
     for i in "${!funcs[@]}"; do
         base=$(basename ${funcs[$i]} .nii.gz)
-        cp -n  MRI/NII/${funcs[$i]} \
+        cp -n  ${SF}/${funcs[$i]} \
             "$BIDS_UNPROC_DEST/func/${bsub}_${bses}_task-${task[$i]}_run-${func_runnr[$i]}_bold.nii.gz"
         # if there are json files copy them too
-        if [ -f MRI/NII/${base}.json]; then
-            cp -n  MRI/NII/${base}.json  \
+        if [ -f ${SF}/${base}.json ]; then
+            cp -n  ${SF}/${base}.json  \
                 "$BIDS_UNPROC_DEST/func/${bsub}_${bses}_task-${task[$i]}_run-${func_runnr[$i]}_bold.json"
         fi
-done
+    done
+fi
 
-
-echo "Copying topups (if they exist)"
+echo "Copying topups (if they exist) ---"
 if [ ${#topup[@]} -eq 0 ]; then
+    echo 'No TOPUPS declared'
+else
+    echo 'TOPUPs'    
     mkdir -p "$BIDS_UNPROC_DEST/fmap"
     for i in "${!topup[@]}"; do
         base=$(basename ${topup[$i]} .nii.gz)
-        cp -n  MRI/NII/${topup[$i]} \
+        cp -n  ${SF}/${topup[$i]} \
             "$BIDS_UNPROC_DEST/fmap/${bsub}_${bses}_run-${func_runnr[$i]}_epi.nii.gz"
         # if there are json files copy them too
-        if [ -f MRI/NII/${base}.json]; then
-            cp -n  MRI/NII/${base}.json  \
+        if [ -f ${SF}/${base}.json ]; then
+            cp -n  ${SF}/${base}.json  \
                 "$BIDS_UNPROC_DEST/fmap/${bsub}_${bses}_run-${func_runnr[$i]}_epi.json"
         fi
-done
-
+    done
+fi
 
 # ==========================================================================
 #   ___      _             _           _              
@@ -278,13 +301,17 @@ done
 #                                            |___/ 
 # ==========================================================================
 
-echo "Copying behavioral logs (if they exist)"
+echo "Copying behavioral logs (if they exist) ---"
 if [ ${#behs[@]} -eq 0 ]; then
+    echo 'No behavioral folders declared'
+else
+    echo 'Behavioral files'    
     mkdir -p "$BIDS_UNPROC_DEST/func"
     for i in "${!behs[@]}"; do
-        cp -n  Behavior/${behs[$i]} \
+        cp -n -r Behavior/${behs[$i]} \
             "$BIDS_UNPROC_DEST/func/${bsub}_${bses}_task-${task[$i]}_run-${func_runnr[$i]}_events"
-done
+    done
+fi
 
 # also copy run000 is if exists << This needs to be added manually if needed
 # cp -r -n Behavior/xxxx "$BIDS_UNPROC_DEST/func/${bsub}_${bses}_task-xxx_run-00_events"
@@ -299,10 +326,14 @@ done
 # 
 # ==========================================================================
 
-echo "Copying eye data (if they exist)"
+echo "Copying eye data (if they exist) ---"
 if [ ${#eyes[@]} -eq 0 ]; then
-    mkdir -p "$BIDS_UNPROC_DEST/func"
+    echo 'No Eye files declared'
+else
+    echo 'Eye files'    
+    mkdir -p "$BIDS_DEST/func"
     for i in "${!eyes[@]}"; do
         cp -n  Eye/${eyes[$i]} \
-            "$BIDS_UNPROC_DEST/func/${bsub}_${bses}_task-${task[$i]}_run-${func_runnr[$i]}_recording-eyetrace_physio.tda"
-done
+            "$BIDS_DEST/func/${bsub}_${bses}_task-${task[$i]}_run-${func_runnr[$i]}_recording-eyetrace_physio.tda"
+    done
+fi
