922e3b269203c5eeed3fe1cf5492b92c1e7a5e36
Chris Klink <p.c.klink@gmail.com>
2020-02-06 15:16:21 +0100

    parallel preprocessing

---
+++ code/subcode/bids_preproc_parallel_runs.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+import os                                    # system functions
+import argparse
+import pandas as pd
+import csv
+
+def run_splitpreproc(csv_file, subses,warp):
+    
+    # define log and job folders =========================================
+    job_path = './code/lisa/preproc'
+    logpp_path = './logs/preproc/' + subses
+    logwarp_path = './logs/warp2nmt/' + subses
+
+    # read csv files =====================================================
+    if csv_file is not None:
+        df = pd.read_csv(csv_file)
+        sub=[]; ses=[]; run=[]; ref=[];
+        
+        for index, row in df.iterrows():
+            for r in row.run.strip("[]").split(" "):
+                sub.append(row.subject)
+                ses.append(row.session)
+                run.append(r)
+                if 'refsubject' in df.columns:
+                    ref.append(row.refsubject)
+                else:
+                    ref.append(row.subject)
+    else:
+        print("No csv-file specified. Don't know what data to process.")
+
+
+    # create basefolder for csv files ====================================
+    csv_path = csv_file[0:-4]
+    os.makedirs(csv_path,exist_ok = True)
+
+    # create csv files ===================================================
+    for idx, subj in enumerate(sub):
+        with open(csv_path + '/run' + run[idx] + '.csv',
+                    'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(
+                ["subject","session","run","datatype","refsubject"])
+            writer.writerow(
+                [sub[idx],ses[idx],"[" + run[idx] + "]","[func]",ref[idx]])
+
+    # create job files ===================================================
+    for idx, subj in enumerate(sub):
+        job_path_ses = job_path + '/' + sub[idx] + str(ses[idx])
+        os.makedirs(job_path_ses,exist_ok = True)
+        
+        f = open(job_path_ses + '/job_run' + run[idx] + '.sh' ,'w', newline='') 
+        
+        f.write('#!/bin/bash\n')
+        f.write('#SBATCH -N 1 --ntasks-per-node=16\n')
+        f.write('#SBATCH -t 10:00:00\n') # make sure this enough time!
+        f.write('#SBATCH --mail-type=END\n')
+        f.write('#SBATCH --mail-user=p.c.klink@gmail.com\n\n')
+        f.write('echo job id $SLURM_JOBID\n')
+        f.write('echo job name $SLURM_JOB_NAME\n')
+        f.write('echo submitted by $SLURM_JOB_ACCOUNT\n')
+        f.write('echo from $SLURM_SUBMIT_DIR\n')
+        f.write('echo the allocated nodes are: $SLURM_JOB_NODELIST\n\n')
+        f.write('module load pre2019\n')
+        f.write('module load eb\n')
+        f.write('module load freesurfer\n')
+        f.write('module load fsl/5.08\n')
+        f.write('module load afni\n\n')
+        f.write('source ~/.bash_profile\n')
+        f.write('source ~/.bashrc\n')
+        f.write('umask u+rwx,g+rwx\n')
+        f.write('umask u+rwx,g+rwx\n\n')
+        f.write('export FSLOUTPUTTYPE=NIFTI_GZ\n\n') 
+        f.write('echo ' + sub[idx] + '-' + str(ses[idx]) + ' run' + run[idx] + '\n')
+        f.write('cd ~/NHP-BIDS\n\n')
+        csv_run = csv_path + '/run' + run[idx] + '.csv'
+        logpp_run = logpp_path + '/run' + run[idx] + '.txt'
+        f.write('./code/bids_preprocessing_workflow.py ' + 
+                '--csv ' + csv_run + ' |& \\\n' + '     tee ' + logpp_run + '\n')
+        
+        if warp is True:
+            f.write('wait\n\n')
+            logwarp_run = logwarp_path + '/run' + run[idx] + '.txt'
+            f.write('./code/bids_warp2nmt_workflow.py ' + 
+                    '--csv ' + csv_run + ' |& \\\n' + '     tee ' + logwarp_run + '\n\n')
+            f.write('echo Reached the end of the job-file')
+        
+        f.close()
+        
+        mkexec_str = 'chmod +x ' + job_path_ses + '/job_run' + run[idx] + '.sh'
+        os.system(mkexec_str)
+        
+        submit_str = 'sbatch ' + job_path_ses + '/job_run' + run[idx] + '.sh'
+        os.system(submit_str)
+        # print(submit_str) # here for debugging
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+            description='Split pre-processing in multiple cluster-jobs')
+    parser.add_argument('--csv',
+                        dest='csv_file', default=None,
+                        help='CSV file with subjects, sessions, runs, and refsubject.')
+    parser.add_argument('--subses',
+                        dest='subses', default=None,
+                        help='string identifying subject and session')
+    parser.add_argument('--warp',
+                        dest='warp', default=True,
+                        help='immediately warp to NMT ([True] / Faalse)')
+
+    args = parser.parse_args()
+
+    run_splitpreproc(**vars(args))
